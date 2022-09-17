82fef06acc4f5d5b1a1c66c11a568d98f16e6a46
Maria Munoz Rodriguez <1593maria@gmail.com>
2022-09-17 17:40:20 +0200

    js code qr api

--- js/app.js
+++ js/app.js
@@ -5,4 +5,18 @@ let qrDowloaderInput = document.querySelector('.qr-code button');
 let qrImage = document.querySelector('.qr-code img');
 
 
+qrBotonInput.addEventListener('click', () => { 
+    let qrValue = qrInput.value
+    if (!qrValue) {
+        return
+    } else {
+        qrBotonInput.innerHTML = "Génération de code QR...!"
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}` //https://goqr.me/api/
 
+        qrImage.addEventListener("load", () => {
+					container.classList.add("active")
+					qrBotonInput.innerHTML = "Créer un Code QR"
+				})
+        
+        }
+})
