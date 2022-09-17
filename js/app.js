8e5081b278394f60f23c284da6419118c19f7561
Maria Munoz Rodriguez <1593maria@gmail.com>
2022-09-17 17:18:36 +0200

    html + sass + js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let container = document.querySelector('.container');
+let qrInput = document.querySelector('.form input');
+let qrBotonInput = document.querySelector('.form button');
+let qrDowloaderInput = document.querySelector('.qr-code button');
+let qrImage = document.querySelector('.qr-code img');
+
+
+
